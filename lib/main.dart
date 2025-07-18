10f37cfe1e04f0bb6535630c80f05f25b9b70b3d
Muhammed-Unais <muhammedunaisq@gmail.com>
2025-07-18 16:27:16 +0530

    feat: Complete Home page UI
    - Added search bar to the Home page
    - Implemented footer UI for improved layout and navigation

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         scaffoldBackgroundColor: AppColors.background,
         textTheme: GoogleFonts.interTextTheme(ThemeData.dark().textTheme),
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.submitButton),
       ),
       home: HomePage(),
     );
